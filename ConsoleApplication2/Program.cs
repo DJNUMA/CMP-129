9105e0ab1358bd77d28f9ed46b3fa7b316c59ed1
djnuma <rickkubik@yahoo.com>
2013-09-05 20:11:17 -0400

    Blank program
    Signed-off-by: djnuma <rickkubik@yahoo.com>

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 set of 1-100 every 10 is different colour, 1 set even are different, odd are another.
+            int x = 1;
+
+            switch (A)
+                case 1:
+                    while(x<10)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(x);
+                        x++;
+                    }
+                case 2:
+                    while(x>10 && x<20)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(x);
+                        x++;
+                    }
+                case 3:
+                    while(x>20 && x<30)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(x);
+                        x++;
+                    }
+                case 4:
+                    while(
+            }
+            
+        }
+    }
+}
